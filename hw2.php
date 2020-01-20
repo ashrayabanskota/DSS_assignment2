3f6973198c5ca24967614f06a46a86d9b0eb20ee
ashrayabanskota <57976848+ashrayabanskota@users.noreply.github.com>
2020-01-20 18:10:33 +0545

    Add files via upload

---
+++ hw2.php
@@ -0,0 +1,18 @@
+<?php
+$x=8;
+$y=20;
+switch($a){
+    case 28:
+        echo "sum is".($x+$y);
+    break;
+    case 160:
+        echo "mutiplication is".($x*$y);
+    break;
+    case 12:
+        echo "difference is".($y-$x);
+    break;
+    default:
+        echo "the sum is an integer";
+}
+
+?>
