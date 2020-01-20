b2f312bbcca81181824bd234de1400e476828458
ashrayabanskota <57976848+ashrayabanskota@users.noreply.github.com>
2020-01-20 18:10:06 +0545

    Add files via upload

---
+++ hw1.php
@@ -0,0 +1,32 @@
+<?php
+
+    $x=8;
+    $y=20;
+    if($x<1){
+        echo "the sum is ".($x+$y);
+    
+    }else{
+        echo 8;
+    }
+
+
+?>
+<?php
+$x=8;
+$y=20;
+if($x<1){
+    echo "the difference is ".($y-$x);
+
+}else{
+    echo 8;
+}
+?>
+<?php
+$x=8;
+$y=20;
+if($x>1){
+    echo "the product is ".($x*$y);
+}else{
+    echo 8;
+}
+?>
